bace366832790d5594d993fce4cdd05124a294e4
woyteck <wojciech.hoffmann@gmail.com>
2024-06-11 18:18:09 +0200

    added connecting to peers

--- main.go
+++ main.go
@@ -13,27 +13,17 @@ import (
 
 func main() {
 	makeNode(":3000", []string{})
+	time.Sleep(time.Second)
 	makeNode(":4000", []string{":3000"})
+	time.Sleep(4 * time.Second)
+	makeNode(":5000", []string{":4000"})
 
-	// go func() {
-	// 	for {
-	// 		time.Sleep(2 * time.Second)
-	// 		makeHandshake()
-	// 	}
-	// }()
 	select {}
 }
 
 func makeNode(listenAddr string, bootstrapNodes []string) *node.Node {
 	n := node.New()
-	go n.Start(listenAddr)
-	time.Sleep(time.Second)
-
-	if len(bootstrapNodes) > 0 {
-		if err := n.BootstrapNetwork(bootstrapNodes); err != nil {
-			log.Fatal(err)
-		}
-	}
+	go n.Start(listenAddr, bootstrapNodes)
 
 	return n
 }
--- node/node.go
+++ node/node.go
@@ -33,41 +33,7 @@ func New() *Node {
 	}
 }
 
-func (n *Node) addPeer(c proto.NodeClient, v *proto.Version) {
-	n.peerLock.Lock()
-	defer n.peerLock.Unlock()
-
-	n.logger.Debugw("new peer connected", "addr", n.listenAddr, "from", n.listenAddr, "height", v.Height)
-
-	n.peers[c] = v
-}
-
-func (n *Node) deletePeer(c proto.NodeClient) {
-	n.peerLock.Lock()
-	defer n.peerLock.Unlock()
-	delete(n.peers, c)
-}
-
-func (n *Node) BootstrapNetwork(addrs []string) error {
-	for _, addr := range addrs {
-		c, err := makeNodeClient(addr)
-		if err != nil {
-			return err
-		}
-
-		v, err := c.Handshake(context.Background(), n.getVersion())
-		if err != nil {
-			n.logger.Errorf("handshake error: %s", err)
-			continue
-		}
-
-		n.addPeer(c, v)
-	}
-
-	return nil
-}
-
-func (n *Node) Start(listenAddr string) error {
+func (n *Node) Start(listenAddr string, bootstrapNodes []string) error {
 	n.listenAddr = listenAddr
 	var (
 		opts       = []grpc.ServerOption{}
@@ -80,6 +46,12 @@ func (n *Node) Start(listenAddr string) error {
 	proto.RegisterNodeServer(grpcServer, n)
 
 	n.logger.Infow("node started...", "port", n.listenAddr)
+
+	// boostrap the network with a list of already known nodes in the network
+
+	if len(bootstrapNodes) > 0 {
+		go n.bootstrapNetwork(bootstrapNodes)
+	}
 
 	return grpcServer.Serve(ln)
 }
@@ -101,6 +73,82 @@ func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*p
 	return &proto.Ack{}, nil
 }
 
+func (n *Node) addPeer(c proto.NodeClient, v *proto.Version) {
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+
+	// handle logic where we decide to accept or drop the incoming node connection
+
+	n.peers[c] = v
+
+	for _, addr := range v.PeerList {
+		if addr != n.listenAddr {
+			fmt.Printf("[%s] need to connect with %s\n", n.listenAddr, addr)
+		}
+	}
+
+	n.logger.Debugw(
+		"new peer successfully connected",
+		"we", n.listenAddr,
+		"remoteNode", v.ListenAddr,
+		"height", v.Height,
+	)
+}
+
+func (n *Node) deletePeer(c proto.NodeClient) {
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+	delete(n.peers, c)
+}
+
+func (n *Node) bootstrapNetwork(addrs []string) error {
+	for _, addr := range addrs {
+		c, v, err := n.dialRemoteNode(addr)
+		if err != nil {
+			return err
+		}
+
+		n.addPeer(c, v)
+	}
+
+	return nil
+}
+
+func (n *Node) dialRemoteNode(addr string) (proto.NodeClient, *proto.Version, error) {
+	c, err := makeNodeClient(addr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	v, err := c.Handshake(context.Background(), n.getVersion())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return c, v, nil
+}
+
+func (n *Node) getVersion() *proto.Version {
+	return &proto.Version{
+		Version:    "blocker-0.1",
+		Height:     0,
+		ListenAddr: n.listenAddr,
+		PeerList:   n.getPeerList(),
+	}
+}
+
+func (n *Node) getPeerList() []string {
+	n.peerLock.RLock()
+	defer n.peerLock.RUnlock()
+
+	peers := []string{}
+	for _, version := range n.peers {
+		peers = append(peers, version.ListenAddr)
+	}
+
+	return peers
+}
+
 func makeNodeClient(listenAddr string) (proto.NodeClient, error) {
 	opts := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -111,12 +159,4 @@ func makeNodeClient(listenAddr string) (proto.NodeClient, error) {
 	}
 
 	return proto.NewNodeClient(c), nil
-}
-
-func (n *Node) getVersion() *proto.Version {
-	return &proto.Version{
-		Version:    "blocker-0.1",
-		Height:     0,
-		ListenAddr: n.listenAddr,
-	}
 }
